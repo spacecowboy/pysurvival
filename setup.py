b0e61f32634826b8e0f1aec5705db47ecc83299c
Jonas Kalderstam <jonas@kalderstam.se>
2013-12-11 16:59:26 +0100

    Added requirements and Make install target
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup, Extension
 
 setup(name = 'pysurvival',
-      version = '1.1',
+      version = '1.2',
       description = 'Cox model from R.',
       author = 'Jonas Kalderstam',
       author_email = 'jonas@kalderstam.se',
       url = '',
       packages = ['pysurvival'],
       package_dir = {'pysurvival': 'pysurvival'},
-      requires = ['numpy'],
+      install_requires = ['numpy>=1.7', 'rpy2'],
      )
